fad90f594f3e7a5a69c5c0c8edc4c5a54d958c37
D2ans0 <git@d2ans0.tk>
2024-06-27 11:30:41 +0400

    update proton, add heroic, sunshine (plus support), and global mangohud

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -7,6 +7,8 @@
       allowUnfree = true;
       packageOverrides = pkgs: {
         unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") { config = { allowUnfree = true; }; };
+        proton-ge9-5 = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/29fe096e2030dd8741bb97dc1c06547816ac957f.tar.gz") { config = { allowUnfree = true; }; };
+        proton-ge9-7 = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/ec21888446f41e13dfc79613e30438aca5b232aa.tar.gz") { config = { allowUnfree = true; }; };
 #         nexusmods = import (fetchTarball "https://github.com/matejc/nixpkgs/archive/nexus-mods-app_2.tar.gz") { config = { allowUnfree = true; }; };
       };
     };
@@ -17,30 +19,40 @@
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     gamescopeSession.enable = true;
-    extraCompatPackages = with pkgs; [ unstable.proton-ge-bin ];
+    extraCompatPackages = with pkgs; [ proton-ge9-5.proton-ge-bin proton-ge9-7.proton-ge-bin ];
   };
 
   programs.gamemode.enable = true;
 
   programs.alvr = {
-  enable = true;
-  package = pkgs.unstable.alvr;
-  openFirewall = true;
+    enable = true;
+    package = pkgs.unstable.alvr;
+    openFirewall = true;
   };
   # enable mangohud for all vulkan apps
   hardware.opengl = {
-  extraPackages = with pkgs; [mangohud];
-  extraPackages32 = with pkgs; [mangohud];
+    extraPackages = with pkgs; [mangohud];
+    extraPackages32 = with pkgs; [mangohud];
   };
 
   environment.systemPackages = with pkgs; [
-    prismlauncher
+    prismlauncher           # minecraft launcher
+    heroic
     wineWowPackages.stable
     winetricks
     protontricks
-    gnome.zenity
+    gnome.zenity            # mod manager 2 installer requirement
     p7zip
+    sunshine                # game streaming
+    mangohud                # performance UI for vulkan applications
   ];
+
+  security.wrappers.sunshine = {
+    owner = "root";
+    group = "root";
+    capabilities = "cap_sys_admin+p";
+    source = "${pkgs.sunshine}/bin/sunshine";
+  };
 
   # Xbox controller compatibility
   # hardware.xone.enable = true;
