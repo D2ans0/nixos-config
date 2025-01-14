58216c11d0054dd10685d1945c9f8f0db879e741
D2ans0 <git@d2ans0.tk>
2025-01-15 03:22:27 +0400

    update to prod 565.77 drivers

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -9,6 +9,7 @@
     nvidia_555_58_prod = import (fetchTarball "https://github.com/nixos/nixpkgs/archive/10ed11d6856a7b67b9b2cef5e52af5c7de34b93f.tar.gz") { config = { allowUnfree = true; }; };
     nvidia_560_35_prod = import (fetchTarball "https://github.com/nixos/nixpkgs/archive/3a4ac243bf3ec40a2cd558f1dfcfe540b42b62c6.tar.gz") { config = { allowUnfree = true; }; };
     nvidia_565_57_beta = import (fetchTarball "https://github.com/nixos/nixpkgs/archive/3e31b3e7165672c1b9e04025bb35b35f05c119ce.tar.gz") { config = { allowUnfree = true; }; };
+    nvidia_565_77_prod = import (fetchTarball "https://github.com/nixos/nixpkgs/archive/b0749e8e6fbce843a674d0cdab7777b6d042002d.tar.gz") { config = { allowUnfree = true; }; };
   };
 
 
@@ -28,6 +29,7 @@
 #    package = (pkgs.nvidia_555_52_beta.linuxPackagesFor config.boot.kernelPackages.kernel).nvidiaPackages.beta; # 555.52.02
 #    package = (pkgs.nvidia_555_58_prod.linuxPackagesFor config.boot.kernelPackages.kernel).nvidiaPackages.latest; # 555.58
 #    package = (pkgs.nvidia_560_35_prod.linuxPackagesFor config.boot.kernelPackages.kernel).nvidiaPackages.latest; # 560.35
-    package = (pkgs.nvidia_565_57_beta.linuxPackagesFor config.boot.kernelPackages.kernel).nvidiaPackages.beta; # 565.57
+#    package = (pkgs.nvidia_565_57_beta.linuxPackagesFor config.boot.kernelPackages.kernel).nvidiaPackages.beta; # 565.57
+    package = (pkgs.nvidia_565_77_prod.linuxPackagesFor config.boot.kernelPackages.kernel).nvidiaPackages.latest; # 565.77
   };
 }
