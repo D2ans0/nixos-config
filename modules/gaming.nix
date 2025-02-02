dbd42121227057701b8f2cd09c0e1003a31f148f
D2ans0 <git@d2ans0.tk>
2025-02-02 14:22:38 +0400

    add 9-23, move protontricks to option

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -12,6 +12,7 @@
         proton-ge9-13 = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/a09c9e044bde2d3dc076e914a6883b6ac3a02223.tar.gz") { config = { allowUnfree = true; }; };
         proton-ge9-18 = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/f975c8df9ec0c6ce5b50f8d43e7fdac085081914.tar.gz") { config = { allowUnfree = true; }; };
         proton-ge9-21 = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/37ce591b9853e4a88d7da23e729eb845267c23c9.tar.gz") { config = { allowUnfree = true; }; };
+        proton-ge9-23 = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/984735ce44e9af59ff2a1ec713cb52ee9e4ec845.tar.gz") { config = { allowUnfree = true; }; };
 #         nexusmods = import (fetchTarball "https://github.com/matejc/nixpkgs/archive/nexus-mods-app_2.tar.gz") { config = { allowUnfree = true; }; };
       };
     };
@@ -22,12 +23,9 @@
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     gamescopeSession.enable = true;
-    extraCompatPackages = with pkgs; [ proton-ge9-5.proton-ge-bin proton-ge9-13.proton-ge-bin proton-ge9-18.proton-ge-bin proton-ge9-21.proton-ge-bin];
+    extraCompatPackages = with pkgs; [ proton-ge9-5.proton-ge-bin proton-ge9-13.proton-ge-bin proton-ge9-18.proton-ge-bin proton-ge9-21.proton-ge-bin proton-ge9-23.proton-ge-bin];
 #   unavailable on stable 24.05, wait for 24.11, or switch to unstable to activate
-#    protontricks = {
-#      enable = true;
-#      package = pkgs.unstable.protontricks;
-#    };
+    protontricks.enable = true;
   };
 
   programs.gamemode.enable = true;
@@ -51,7 +49,6 @@
     p7zip
     sunshine                # game streaming
     mangohud                # performance UI for vulkan applications
-    unstable.protontricks
   ];
 
   security.wrappers.sunshine = {
