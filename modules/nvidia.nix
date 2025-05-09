70cdf9434b30210af6b3503ad766f473822b0889
D2ans0 <git@d2ans0.tk>
2025-05-09 22:47:08 +0400

    switch to 575.51.02 beta drivers

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -9,6 +9,14 @@ let
     settingsSha256 = "sha256-XMk+FvTlGpMquM8aE8kgYK2PIEszUZD2+Zmj2OpYrzU=";
     persistencedSha256 = "sha256-G1V7JtHQbfnSRfVjz/LE2fYTlh9okpCbE4dfX9oYSg8=";
   };
+  nvidia_575_51_02_beta = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "575.51.02";
+    sha256_64bit = "sha256-XZ0N8ISmoAC8p28DrGHk/YN1rJsInJ2dZNL8O+Tuaa0=";
+    sha256_aarch64 = "sha256-NNeQU9sPfH1sq3d5RUq1MWT6+7mTo1SpVfzabYSVMVI=";
+    openSha256 = "sha256-NQg+QDm9Gt+5bapbUO96UFsPnz1hG1dtEwT/g/vKHkw=";
+    settingsSha256 = "sha256-6n9mVkEL39wJj5FB1HBml7TTJhNAhS/j5hqpNGFQE4w=";
+    persistencedSha256 = "sha256-gmco+clEIY8bedxHC4wp+fH5JavTzyI1BI8BxoeJJI=";
+  };
 in {
   # Allow unfree, and pin older driver versions
   nixpkgs.config.allowUnfree = true;
@@ -41,6 +49,7 @@ in {
 #    package = (pkgs.nvidia_565_77_prod.linuxPackagesFor config.boot.kernelPackages.kernel).nvidiaPackages.beta; # 565.57
 #    package = (pkgs.nvidia_570_133_prod.linuxPackagesFor config.boot.kernelPackages.kernel).nvidiaPackages.production; # 570.133
 #    package = config.boot.kernelPackages.nvidiaPackages.production;
-    package = nvidia_570_133_07;
+#    package = nvidia_570_133_07;
+    package = nvidia_575_51_02_beta;
   };
 }
