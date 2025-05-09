c8e97bf7fa1b6824e966f2c6df6dbcf71a234632
D2ans0 <git@d2ans0.tk>
2025-05-09 22:46:42 +0400

    add nofail to mounts

--- modules/mounts.nix
+++ modules/mounts.nix
@@ -15,18 +15,18 @@
   fileSystems."/run/media/d2/2TB NVME" = {
    device = "/dev/disk/by-partuuid/3b438142-5c1a-440e-9787-1e7e593f114e";
     fsType = "ntfs-3g";
-    options = [ "rw" "nosuid" "nodev" "uid=1000" "gid=100" ];
+    options = [ "rw" "nosuid" "nodev" "nofail" "uid=1000" "gid=100" ];
   };
 
   fileSystems."/run/media/d2/WDBlue 7200RPM" = {
     device = "/dev/disk/by-partuuid/9535318d-4a9c-4f10-bdd7-722c03ba832e";
     fsType = "ntfs-3g";
-    options = [ "rw" "nosuid" "nodev" "uid=1000" "gid=100" ];
+    options = [ "rw" "nosuid" "nodev" "nofail" "uid=1000" "gid=100" ];
   };
 
   fileSystems."/run/media/d2/ADATA 256GB" = {
     device = "/dev/disk/by-partuuid/46e55d62-d889-493e-8c4a-1ff0e8f9afb5";
     fsType = "ntfs-3g";
-    options = [ "rw" "nosuid" "nodev" "uid=1000" "gid=100" ];
+    options = [ "rw" "nosuid" "nodev" "nofail" "uid=1000" "gid=100" ];
   };
 }
