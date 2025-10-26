63ae2afd72dbb0989ed84d041ba8fbae8998b33b
D2ans0 <git@d2ans0.tk>
2025-10-26 15:12:03 +0400

    set low swappiness

--- configuration.nix
+++ configuration.nix
@@ -19,8 +19,17 @@
     ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+#  boot.loader.systemd-boot.enable = true;
+#  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernel.sysctl = {
+      "vm.swappiness" = 5;
+    };
+  };
 
   networking.hostName = "stumper"; # Define your hostname.
 
