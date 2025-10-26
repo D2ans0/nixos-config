ec30832e5c62a94d3feb4f7afd2446c43d85d890
D2ans0 <git@d2ans0.tk>
2025-10-26 15:12:27 +0400

    clean up old proton versions

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -3,14 +3,7 @@
   nixpkgs = {
     config = {
       allowUnfree = true;
-      packageOverrides = pkgs: {
-        ge9-5 = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/29fe096e2030dd8741bb97dc1c06547816ac957f.tar.gz") {};
-        ge9-13 = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/a09c9e044bde2d3dc076e914a6883b6ac3a02223.tar.gz") {};
-        ge9-18 = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/f975c8df9ec0c6ce5b50f8d43e7fdac085081914.tar.gz") {};
-        ge9-25 = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/5168db408888db34667e8e7dccfe85e568c4349c.tar.gz") {};
-        ge9-27 = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/c92c1efa465497446d090134c6c8441c5fb9c4b2.tar.gz") {};
-        ge10-12 = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4f3e0b708cadf6ebe60653f8eba3e3c10a7071f3.tar.gz") {};
-      };
+      packageOverrides = pkgs: {};
     };
   };
 
@@ -21,13 +14,6 @@
     gamescopeSession.enable = true;
     extraCompatPackages = with pkgs; [
       proton-ge-bin
-      ge9-5.proton-ge-bin
-      ge9-13.proton-ge-bin
-      ge9-18.proton-ge-bin
-      (ge9-25.proton-ge-bin.override { steamDisplayName = "GE-Proton-9.25"; })
-      (ge9-27.proton-ge-bin.override { steamDisplayName = "GE-Proton-9.27"; })
-      (ge10-12.proton-ge-bin.override { steamDisplayName = "GE-Proton-10.12"; })
-
     ];
     protontricks.enable = true;
   };
