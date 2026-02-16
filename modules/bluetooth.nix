3820493900ff29669de1c79dfd895cd1eb0be75f
D2ans0 <git@d2ans0.tk>
2026-02-16 18:15:19 +0400

    revert LE behavior to default

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -11,11 +11,6 @@
         class = "0x000100";
         FastConnectable = true;
     };
-    LE = {
-      MinConnectionInterval = 7;
-      MaxConnectionInterval = 9;
-      ConnectionLatency = 0;
-    };
   };
   hardware.bluetooth.input.General = { UserspaceHID = true; };
   boot.kernelParams = [ "btusb.enable_autosuspend=0" ];
