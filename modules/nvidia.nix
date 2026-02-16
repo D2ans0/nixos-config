d4dbd417ec9a49fdf3825fd88b5e12cec2d09d49
D2ans0 <git@d2ans0.tk>
2026-02-16 18:11:30 +0400

    update to 580.95.05

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -26,6 +26,17 @@ let
     persistencedSha256 = "sha256-bs3bUi8LgBu05uTzpn2ugcNYgR5rzWEPaTlgm0TIpHY=";
   };                                                                           
 
+  nvidia_580_95_05 = config.boot.kernelPackages.nvidiaPackages.mkDriver { 
+    version = "580.95.05";
+    sha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
+    sha256_aarch64 = "sha256-zLRCbpiik2fGDa+d80wqV3ZV1U1b4lRjzNQJsLLlICk=";
+    openSha256 = "sha256-RFwDGQOi9jVngVONCOB5m/IYKZIeGEle7h0+0yGnBEI=";
+    settingsSha256 = "sha256-F2wmUEaRrpR1Vz0TQSwVK4Fv13f3J9NJLtBe4UP2f14=";
+    persistencedSha256 = "sha256-QCwxXQfG/Pa7jSTBB0xD3lsIofcerAWWAHKvWjWGQtg=";
+  };
+
+
+
 in {
   # Allow unfree, and pin older driver versions
   nixpkgs.config.allowUnfree = true;
@@ -42,6 +53,6 @@ in {
     open = false;
     nvidiaSettings = true;
 #    package = config.boot.kernelPackages.nvidiaPackages.production;
-    package = nvidia_580_76_05;
+    package = nvidia_580_95_05;
   };
 }
