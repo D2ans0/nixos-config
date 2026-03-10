bdea77e34df02498c1bbd38f13080824d47013b4
D2ans0 <git@d2ans0.tk>
2026-03-11 00:06:37 +0400

    remove kvm vbox backend... I gotta read the readmes man

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -2,8 +2,6 @@
 { 
     virtualisation.virtualbox.host = {
       enable = true;
-      enableKvm = true;
-      addNetworkInterface = false;
     };
     users.extraGroups.vboxusers.members = [ "d2" ];
     virtualisation.docker.enable = true;
